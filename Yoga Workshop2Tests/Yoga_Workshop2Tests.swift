64aacd50ce600b51cfaa0c633688ab3108ff2e72
Karrin Burns <karrinburns@comcast.net>
2018-04-02 05:46:34 -0500

    Initial Commit

---
+++ Yoga Workshop2Tests/Yoga_Workshop2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Yoga_Workshop2Tests.swift
+//  Yoga Workshop2Tests
+//
+//  Created by BurnsK on 4/2/18.
+//  Copyright © 2018 BurnsK. All rights reserved.
+//
+
+import XCTest
+@testable import Yoga_Workshop2
+
+class Yoga_Workshop2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
